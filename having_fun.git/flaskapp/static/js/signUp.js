54c0220abcb5f08755765e25d0814f64d67b4871
Nikhil Sharma <nkhl681@gmail.com>
2017-07-11 10:08:36 -0400

    Update signUp.js

--- having_fun.git/flaskapp/static/js/signUp.js
+++ having_fun.git/flaskapp/static/js/signUp.js
@@ -13,4 +13,38 @@ $(function(){
 			}
 		});
 	});
+	$('#signinBtn').click(function()
+          {
+            auth2.grantOfflineAccess({'redirect_uri': 'postmessage'}).then(signInCallback);
+           });
+
+	 function signInCallback(json) {
+		console.log('inside callback fuction');
+		console.log(json);
+		authResult = json;
+		if (authResult['code']) {
+			$('#signinBtn').attr('style', 'display: none');
+             // $('#result').html('One-Time Auth Code:</br>'+ authResult['code'] + '<br>Now json'+JSON.stringify(json))
+             // Send the code to the server
+			$.ajax({
+			type: 'POST',
+			url: '/oauth/send',
+			processData: false,
+			data: authResult['code'],
+      //contentType: 'application/json;charset=UTF-8',
+			contentType: 'application/octet-stream; charset=utf-8',
+			success: function(result) {
+       
+			if (result) { 
+                console.log('Login Successful!</br>'+ JSON.stringify(result )+'and key'+authResult['code']) 
+			} 
+			else if (authResult['error']) {
+				console.log('There was an error: ' + authResult['error']);
+			}
+				}
+					});
+						}
+							}
+
+
 });
